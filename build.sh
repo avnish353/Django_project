c220dea14fd6ffebc429e8084b7382658755147d
avnish353 <avnishsharma6268@gmail.com>
2026-01-10 15:18:53 +0530

    Add build.sh and data.json

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 #!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
+>>>>>>> a90443b (Add build.sh and data.json)
 python manage.py loaddata data.json
